4046c175083e004af23292d628968501cea5ca69
Antonio Gomez <josuegomez082004@gmail.com>
2026-03-27 11:17:31 -0600

    Se agrego una linea de documentacion a README y tambien se agrego una nueva condicion

--- archivo1.py
+++ archivo1.py
@@ -1,3 +1,6 @@
+from itertools import count
+
+
 print("Hola mundo, escrito desde python y el cual será actualizado en git-hub") #un simple print
 
 listaNumber1  = [1,2,3,4,5,6,7,8,9,10] # una cadena de numeros
@@ -7,3 +10,8 @@ for number in listaNumber1: # ciclo para mostrar los numeros
     contador = contador + 1 # incrementa el contador
 
 print("Este es un print escrito desde GitHub") # imprime una linea de texto
+
+if(len(listaNumber1) <= 10):
+    print("su lista tiene 10 o menos numeros")
+else:
+    print("su lista tiene mas de 10 numeros")
