29f531003b9aa8141dcfb37352272a75cbed8101
Antonio Gomez <josuegomez082004@gmail.com>
2026-03-27 10:28:23 -0600

    Primer Commit Realizado en la Prueba

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RepoPrueba
 Este es un repositorio de prueba :)
+
+Cambio en la documentacion: Se agrego un archivo .py
---
+++ archivo1.py
@@ -0,0 +1,7 @@
+print("Hola mundo, escrito desde python y el cual será actualizado en git-hub")
+
+listaNumber1  = [1,2,3,4,5,6,7,8,9,10]
+contador = 0
+for number in listaNumber1:
+    print(f"{contador}: numero ingresado: {number}")
+    contador = contador + 1
